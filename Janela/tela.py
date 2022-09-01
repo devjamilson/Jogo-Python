19f0abae51d806cb34d1eaa79d8ffd08b1929205
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2022-09-01 20:56:21 -0300

    movimentando objetos

--- Janela/tela.py
+++ Janela/tela.py
@@ -7,12 +7,32 @@ pygame.init()
 largura = 1366
 altura = 710
 
+x = largura/2
+y = altura/2
+
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Joguinho sem nome ainda')
 
+relogio = pygame.time.Clock()
+
 while True:
+    relogio.tick(30)
+    tela.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+        
+
+        '''controlando um objeto'''
+        if pygame.key.get_pressed()[K_1]:
+            x = x - 20
+        if pygame.key.get_pressed()[K_3]:
+            x = x + 20
+         
+
+
+    pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
     pygame.display.update()
